e7409e6c8efbacd5f208031dc16753e3d729910e
Juuso Minkkilä <K1756@student.jamk.fi>
2017-09-07 17:26:18 +0300

    Finished the project, added a demo video to '/screenshots' subfolder

--- ListViewExcersice/app/src/main/java/com/example/k1756/listviewexcersice/DetailActivity.java
+++ ListViewExcersice/app/src/main/java/com/example/k1756/listviewexcersice/DetailActivity.java
@@ -1,7 +1,11 @@
 package com.example.k1756.listviewexcersice;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.k1756.listviewexcersice.R;
 
@@ -13,6 +17,30 @@ public class DetailActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Draw activity detail layout
         setContentView(R.layout.activity_detail);
+
+        // Get intent
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String phone = bundle.getString("phone");
+
+        // Update text and image views to show data
+        TextView textView = (TextView) findViewById(R.id.phoneTextView);
+        textView.setText(phone);
+        ImageView imageView = (ImageView) findViewById(R.id.phoneImageView);
+        switch (phone) {
+            case "Android": imageView.setImageResource(R.drawable.android); break;
+            case "iPhone": imageView.setImageResource(R.drawable.ios); break;
+            case "WindowsMobile": imageView.setImageResource(R.drawable.windows); break;
+            case "Blackberry": imageView.setImageResource(R.drawable.blackberry); break;
+            case "WebOS": imageView.setImageResource(R.drawable.webos); break;
+            case "Ubuntu": imageView.setImageResource(R.drawable.ubuntu); break;
+        }
+    }
+
+    public void backButtonPressed(View view) {
+        // finish and close activity
+        finish();
     }
 }
