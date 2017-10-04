b8ad05a49701730f5cfc4c05eef6bb8eec408de6
K1756 <K1756@student.jamk.fi>
2017-10-04 10:24:42 +0300

    Developed the ad test

--- AdTest/app/src/main/java/com/example/k1756/adtest/MainActivity.java
+++ AdTest/app/src/main/java/com/example/k1756/adtest/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.k1756.adtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
+    private AdView myAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        myAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        myAdView.loadAd(adRequest);
     }
 }
