7d0940e77b2253a726532acaac202413f9f426fe
K1756 <K1756@student.jamk.fi>
2017-10-23 10:29:05 +0300

    New stuff lol

---
+++ ViewPagerexample/app/src/main/java/com/example/k1756/viewpagerexample/SomeFragment.java
@@ -0,0 +1,22 @@
+package com.example.k1756.viewpagerexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by K1756 on 2017-10-23.
+ */
+
+public class SomeFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_some, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("Some Fragment Content");
+        return rootView;
+    }
+}
